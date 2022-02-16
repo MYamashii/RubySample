5651ecd5225e7550e9eeb3e830f8a32014b2a8f8
Masato Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2022-02-16 21:22:13 +0900

    Move simplecov at the top in spec_helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
+require 'simplecov'
+SimpleCov.start
+
+require 'simplecov-cobertura'
+SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
 
 require 'rspec'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..'))
 
 require 'job_preparer'
-
-require 'simplecov'
-SimpleCov.start
-
-require 'simplecov-cobertura'
-SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
