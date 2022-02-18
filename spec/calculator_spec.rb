1f100f7303079ab158afe8290512c7998077ac50
Masato Yamashita <4653960+M-Yamashita01@users.noreply.github.com>
2022-02-19 07:22:47 +0900

    Add test.

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe Calculator do
   describe '#sum' do
     it { expect(described_class.new.sum(1, 2)).to eq(3) }
   end
+
+  describe '#multipy' do
+    it { expect(described_class.new.multipy(1, 2)).to eq(2) }
+  end
 end
